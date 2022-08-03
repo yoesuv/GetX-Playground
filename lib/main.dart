e7da9d790a3ea00fcafd20e508d0d4f144c80d26
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2022-08-03 14:53:40 +0700

    :pencil: setup basic alarm service

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,19 @@
+import 'dart:isolate';
+
+import 'package:android_alarm_manager_plus/android_alarm_manager_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:my_getx_playground/src/my_app.dart';
 
-void main() {
+void main() async{
   WidgetsFlutterBinding.ensureInitialized();
+  await AndroidAlarmManager.initialize();
   runApp(const MyApp());
+  const int helloAlarmID = 0;
+  await AndroidAlarmManager.periodic(const Duration(minutes: 1), helloAlarmID, printHelloWorld);
+}
+
+void printHelloWorld() {
+  final now = DateTime.now();
+  final int isolatedId = Isolate.current.hashCode;
+  debugPrint('Main # $now, Hello World!, isolated $isolatedId function $printHelloWorld');
 }
