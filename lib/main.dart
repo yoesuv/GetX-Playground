ab6c583e81931ed6e061410e629091ea18cc5ced
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2022-08-04 14:51:37 +0700

    :art: setup local notification

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'dart:isolate';
 import 'package:android_alarm_manager_plus/android_alarm_manager_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:my_getx_playground/src/core/data/constants.dart';
+import 'package:my_getx_playground/src/core/notifications/notification_api.dart';
 import 'package:my_getx_playground/src/my_app.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -17,6 +18,7 @@ void main() async {
     final strDate = prefs.getString(keyMyAlarm);
     if (strDate != null || strDate!.isNotEmpty) {
       final alarmDate = DateTime.parse(strDate);
+      debugPrint('Main # setup alarm at $alarmDate');
       const int helloAlarmID = 0;
       await AndroidAlarmManager.oneShotAt(
         alarmDate,
@@ -35,5 +37,6 @@ void printHelloWorld() async {
   debugPrint(
       'Main # $now, Hello World!, isolated $isolatedId function $printHelloWorld');
   final prefs = await SharedPreferences.getInstance();
+  NotificationApi.showNotification(title: 'Alarm', body: '$now');
   prefs.setString(keyMyAlarm, '');
 }
