27942c090364581419ebfe19394ad930eaabe11c
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2022-08-27 09:26:36 +0700

    :art: UI dialog input new task

--- lib/src/menu/storage/storage_screen.dart
+++ lib/src/menu/storage/storage_screen.dart
@@ -2,53 +2,90 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:my_getx_playground/src/menu/storage/storage_controller.dart';
 
+const double contentDialogPadding = 12;
+
 class StorageScreen extends GetView<StorageController> {
   static const routeName = '/my_task';
   const StorageScreen({Key? key}) : super(key: key);
 
   void _showInputDialog() {
-    Get.defaultDialog(
-      title: 'Insert New Task',
-      contentPadding: const EdgeInsets.all(0),
-      content: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          const TextField(
-            decoration: InputDecoration(
-              hintText: 'Task Title',
-            ),
-          ),
-          const SizedBox(height: 8),
-          const TextField(
-            decoration: InputDecoration(
-              hintText: 'Task Content',
-            ),
-          ),
-          const SizedBox(height: 16),
-          Row(
-            children: [
-              Expanded(
-                child: InkWell(
-                  onTap: () {
-                    Get.back();
-                  },
-                  child: Text('Cancel'),
+    Get.dialog(
+      AlertDialog(
+        contentPadding: EdgeInsets.zero,
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Container(
+              color: Colors.teal,
+              padding: const EdgeInsets.symmetric(vertical: 12),
+              child: const Center(
+                child: Text(
+                  'Insert New Task',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.white,
+                  ),
                 ),
               ),
-              Expanded(
-                child: InkWell(
-                  onTap: () {
-                    Get.back();
-                  },
-                  child: Text('Save'),
+            ),
+            const SizedBox(height: 8),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: contentDialogPadding),
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Task Title',
                 ),
               ),
-            ],
-          ),
-        ],
+            ),
+            const SizedBox(height: 8),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: contentDialogPadding),
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Task Content',
+                ),
+              ),
+            ),
+            const SizedBox(height: 16),
+            Padding(
+              padding: const EdgeInsets.only(
+                bottom: contentDialogPadding,
+                left: contentDialogPadding,
+                right: contentDialogPadding,
+              ),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: OutlinedButton(
+                      onPressed: () {
+                        Get.back();
+                      },
+                      style: OutlinedButton.styleFrom(
+                        tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                      ),
+                      child: const Text('Cancel'),
+                    ),
+                  ),
+                  const SizedBox(width: 8),
+                  Expanded(
+                    child: OutlinedButton(
+                      onPressed: () {
+                        Get.back();
+                      },
+                      style: OutlinedButton.styleFrom(
+                        tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                      ),
+                      child: const Text('Save'),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
       barrierDismissible: false,
-      radius: 8,
     );
   }
 
