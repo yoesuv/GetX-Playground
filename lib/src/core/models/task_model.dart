31a6e2f3e27c5e6256e7b9f01e849d6b3025542b
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2022-08-29 11:34:15 +0700

    :pencil: to string

--- lib/src/core/models/task_model.dart
+++ lib/src/core/models/task_model.dart
@@ -10,14 +10,19 @@ class TaskModel {
   String? contentTask;
 
   factory TaskModel.fromJson(Map<String, dynamic> json) => TaskModel(
-    id: json["id"],
-    titleTask: json["titleTask"],
-    contentTask: json["contentTask"],
-  );
+        id: json["id"],
+        titleTask: json["titleTask"],
+        contentTask: json["contentTask"],
+      );
 
   Map<String, dynamic> toJson() => {
-    "id": id,
-    "titleTask": titleTask,
-    "contentTask": contentTask,
-  };
+        "id": id,
+        "titleTask": titleTask,
+        "contentTask": contentTask,
+      };
+
+  @override
+  String toString() {
+    return toJson().toString();
+  }
 }
