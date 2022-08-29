e61f074218a9a0e5d3525329a214e704a2a95c56
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2022-08-29 11:39:31 +0700

    :construction: save list as map

--- lib/src/core/data/constants.dart
+++ lib/src/core/data/constants.dart
@@ -1,3 +1,4 @@
 const String keyMyAlarm = 'key_my_alarm';
 const int idMyAlarm = 1;
 const String myStorage = 'get_x_playground_storage';
+const String myTask = 'my_task';
--- lib/src/menu/storage/storage_controller.dart
+++ lib/src/menu/storage/storage_controller.dart
@@ -1,14 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:my_getx_playground/src/core/data/constants.dart';
 import 'package:my_getx_playground/src/core/models/task_model.dart';
 import 'package:my_getx_playground/src/utils/app_snackbar.dart';
 
+// https://stackoverflow.com/a/66739056
 class StorageController extends GetxController {
+  final box = GetStorage();
   final title = 'Get Storage'.obs;
   final tasks = <TaskModel>[].obs;
 
   final taskTitleController = TextEditingController();
   final taskContentController = TextEditingController();
+
+  @override
+  void onInit() {
+    super.onInit();
+    final String? dbTask = box.read<String>(myTask);
+    debugPrint('StorageController # on init $dbTask', wrapWidth: 1024);
+  }
 
   void insertTask() {
     debugPrint('StorageController # insert task');
@@ -22,8 +33,8 @@ class StorageController extends GetxController {
         titleTask: taskTitleController.text,
         contentTask: taskContentController.text,
       );
-      debugPrint('StorageController # task ${task.toJson()}');
       tasks.add(task);
+      box.write(myTask, tasks.asMap().toString());
       _clearTextController();
     }
   }
